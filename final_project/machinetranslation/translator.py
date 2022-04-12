389fc59d41b88ec3ee3c40740acc6f226e67a421
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-04-12 03:37:31 +0000

    translation instance added

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,4 +1,5 @@
 """Language Translator"""
+import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
@@ -6,11 +7,27 @@ from dotenv import load_dotenv
 load_dotenv()
 APIKEY = os.environ['API_KEY']
 URL = os.environ['URL']
-def englishToFrench(englishText):
+
+
+authenticator = IAMAuthenticator(APIKEY)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('{url}')
+
+def translator(texttotranslate):
+    # This function translates text from one language to another
+    translation = language_translator.translate(
+    text=texttotranslate,
+    model_id='en-fr').get_result()
+    return json.dumps(translation, indent=2, ensure_ascii=False)
+def englishToFrench(englishtext):
     """English to French Translation"""
-    frenchText=""
-    return frenchText
-def frenchToEnglish(frenchText):
+    frenchtext=translator(englishtext)
+    return frenchtext
+def frenchToEnglish(frenchtext):
     """French to English Translation"""
-    englishText=""
-    return englishText
+    englishtext=translator(frenchtext)
+    return englishtext
