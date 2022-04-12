c7da79a096fd3d75dc3cd262848f45c4d894eee9
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-04-12 06:49:10 +0000

    testfiles edited

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,9 +2,9 @@ import unittest
 from translator import languagetranslator, englishtofrench, frenchtoenglish
 class EnglishtoFrench(unittest.TestCase):
     def englishtofrenchtest(self): #This function translates english to french
-        self.assertEqual(englishtofrench('','Please enter some text to translate'))
-        self.assertEqual(englishtofrench('Hello','Bonjour'))
+        self.assertEqual(englishtofrench(''),'Please enter some text to translate')
+        self.assertEqual(englishtofrench('Hello'),'Bonjour')
 class FrecnchtoEnglish(unittest.TestCase):
     def frenchtoenglishtest(self): #This function translatesfrench to english
-        self.assertEqual(frenchtoenglish('','Please enter some text to translate'))
-        self.assertEqual(frenchtoenglish('Bojour','Hello'))
+        self.assertEqual(frenchtoenglish(''),'Please enter some text to translate')
+        self.assertEqual(frenchtoenglish('Bojour'),'Hello')
--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,10 +1,10 @@
 import unittest
-from translator import languagetranslator, englishtoFrench, frenchtoEnglish
+from translator import languagetranslator, englishtofrench, frenchtoenglish
 class EnglishtoFrench(unittest.TestCase):
     def englishtofrenchtest(self): #This function translates english to french
-        self.assertEqual(englishtofrench('','Please enter some text to translate'))
-        self.assertEqual(englishtofrench('Hello','Bonjour'))
+        self.assertEqual(englishtofrench(''),'Please enter some text to translate')
+        self.assertEqual(englishtofrench('Hello'),'Bonjour')
 class FrecnchtoEnglish(unittest.TestCase):
     def frenchtoenglishtest(self): #This function translatesfrench to english
-        self.assertEqual(frenchtoenglish('','Please enter some text to translate'))
-        self.assertEqual(frenchtoenglish('Bojour','Hello'))
+        self.assertEqual(frenchtoenglish(''),'Please enter some text to translate')
+        self.assertEqual(frenchtoenglish('Bojour'),'Hello')
