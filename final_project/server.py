020fbd03c1e23b1f19f4e3f80eb56b58c2b699a5
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-04-13 11:09:54 +0000

    translation completed

--- final_project/server.py
+++ final_project/server.py
@@ -1,7 +1,8 @@
-from machinetranslation import translator
-from flask import Flask, render_template, request
 import json
 import machinetranslation
+from machinetranslation import translator
+
+from flask import Flask, render_template, request
 
 
 app = Flask("Web Translator")
@@ -9,15 +10,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    translatedtext=translator.frenchtoenglish(textToTranslate)
-    return translatedtext
+    #Write your code here
+    returnedvalue=translator.englishtofrench(textToTranslate)
+    return returnedvalue
+    #return textToTranslate
+
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    translatedtext=translator.frenchtoenglish(textToTranslate)
-    return translatedtext
+    returnedvalue=translator.frenchtoenglish(textToTranslate)
+    return returnedvalue
+    
     
 @app.route("/")
 def renderIndexPage():
