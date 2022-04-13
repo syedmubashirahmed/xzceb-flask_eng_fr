60201ea419250521e2c200aad3a5de11625b9fd2
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-04-13 07:32:53 +0000

    translator function updated

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -5,23 +5,34 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 load_dotenv()
-APIKEY=os.environ['API_KEY']
-URL=os.environ['URL']
+APIKEY=os.environ["apikey"]
+URL=os.environ['url']
 authenticator=IAMAuthenticator(APIKEY)
 language_translator=LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
 language_translator.set_service_url(URL)
-def languagetranslator(texttotranslate):
-    """This function translates text from one language to another"""
-    translation = language_translator.translate(
-    text=texttotranslate, model_id='en-fr').get_result()
-    if texttotranslate == '': 
-        return "Please enter some text to translate."
-    return json.dumps(translation, indent=2, ensure_ascii=False)
 def englishtofrench(englishtext):
     """English to French Translation"""
-    frenchtext=languagetranslator(englishtext)
-    return frenchtext
+    returnvalue=''
+    if englishtext !='':
+        translation=language_translator.translate(
+            text=englishtext,model_id='en-fr').get_result()
+        todictionary=json.loads(translation)
+        returnvalue=(todictionary["translations"][0]["translation"])
+    else:
+        returnvalue="Value entered cannot be null"
+    return returnvalue
 def frenchtoenglish(frenchtext):
     """French to English Translation"""
-    englishtext = languagetranslator(frenchtext)
-    return englishtext
+    returnvalue=''
+    if frenchtext !='':
+        translation=language_translator.translate(
+             text=frenchtext,model_id='fr-en').get_result()
+        todictionary=json.loads(translation)
+        returnvalue=(todictionary["translation"][0]["translation"])
+    else:
+        returnvalue="Value entered cannot be null"
+    return returnvalue
+print(englishtofrench('hello'))
+print(frenchtoenglish('Bonjour'))
+print(englishtofrench(''))
+print(frenchtoenglish(''))
