22ad07819a4b1586786111f108e6fe93ccb393b8
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-04-15 12:19:07 +0000

    tests ok

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,6 +2,7 @@
 #  apikey="2n9ksWcmASg1fqufKsysdIjLr-qQvepVXDKDM3TSk_nO"
  # url="https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/e6d3bba4-30c2-4ede-8ec0-fd92271c3d93"
 import json
+import unittest
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -14,24 +15,34 @@ language_translator=LanguageTranslatorV3(version='2018-05-01', authenticator=aut
 language_translator.set_service_url(URL)
 def englishtofrench(englishtext):
     """English to French Translation"""
-    returnvalue=''
-    if englishtext!='':
-        translation=language_translator.translate(
-            text=englishtext,model_id='en-fr').get_result()
-        returnvalue=(translation["translations"][0]["translation"])
-    else:
-        returnvalue="Text entered is invalid"
+   
+    translation=language_translator.translate(
+        text=englishtext,model_id='en-fr').get_result()
+    returnvalue=(translation["translations"][0]["translation"])
+   
+   
     return returnvalue
 def frenchtoenglish(frenchtext):
     """French to English Translation"""
-    returnvalue=''
-    if frenchtext !='':
-        translation=language_translator.translate(
-             text=frenchtext,model_id='fr-en').get_result()
-        returnvalue=(translation["translations"][0]["translation"])
-    else:
-        returnvalue="Text entered is invalid"
+    
+    
+    translation=language_translator.translate(
+        text=frenchtext,model_id='fr-en').get_result()
+    
+    returnvalue=(translation["translations"][0]["translation"])
+   
+    
     return returnvalue
     
-#print(englishtofrench("hello how are you"))
-#print(frenchtoenglish("Bonjour"))
+
+class EnglishtoFrench(unittest.TestCase):
+    def testenglishtofrench(self): #This function translates english to french
+       # self.assertEqual(englishtofrench(''),'Text entered is invalid')
+        self.assertEqual(englishtofrench('Hello'),'Bonjour')
+        self.assertNotEqual(englishtofrench('Hello'),'Hello')
+class FrecnchtoEnglish(unittest.TestCase):
+    def testfrenchtoenglish(self): #This function translatesfrench to english
+        #self.assertEqual(frenchtoenglish(''),'Text entered is invalid')
+        self.assertEqual(frenchtoenglish('Bonjour'),'Hello')
+        self.assertNotEqual(frenchtoenglish('Bonjour'),'Bonjour')
+unittest.main()
--- final_project/test.py
+++ final_project/test.py
@@ -5,7 +5,10 @@ class EnglishtoFrench(unittest.TestCase):
     def englishtofrenchtest(self): #This function translates english to french
         self.assertEqual(translator.englishtofrench(''),'Text entered is invalid')
         self.assertEqual(translator.englishtofrench('Hello'),'Bonjour')
+        self.assertNotEqual(translator.englishtofrench('Hello'),'Hello')
 class FrecnchtoEnglish(unittest.TestCase):
     def frenchtoenglishtest(self): #This function translatesfrench to english
         self.assertEqual(translator.frenchtoenglish(''),'Text entered is invalid')
         self.assertEqual(translator.frenchtoenglish('Bojour'),'Hello')
+        self.assertNotEqual(translator.frenchtoenglish('Bonjour'),'Bonjour')
+unittest.main()
