91e37d6d40ef13b5969b9b0601107dfb8c4fac1b
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-04-15 17:57:24 +0000

    App-function-proerly

--- final_project/server.py
+++ final_project/server.py
@@ -4,13 +4,13 @@ app = Flask("Web Translator")
 @app.route("/englishtofrench")
 def englishtofrench():
     """English to French"""
-    texttotranslate = request.args.get('textToTranslate')
+    texttotranslate = request.args.get('texttotranslate')
     returnedvalue=translator.englishtofrench(texttotranslate)
     return f"<h1>The translation of {texttotranslate} in french  is: {returnedvalue}</h1>"
 @app.route("/frenchtoenglish")
 def frenchtoenglish():
     """French to English"""
-    texttotranslate = request.args.get('textToTranslate')
+    texttotranslate = request.args.get('texttotranslate')
     returnedvalue=translator.frenchtoenglish(texttotranslate)
     return f"<h1>The translation of {texttotranslate} in English is: {returnedvalue} </h1>"
 @app.route("/")
